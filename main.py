9729b5b770078321c7ceb9aea613fb26fb0c584e
Kavish Gupta <kavishg36@gmail.com>
2026-02-06 15:03:50 +0530

    Model Training

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationnConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationnConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 if __name__=="__main__":
@@ -15,18 +16,28 @@ if __name__=="__main__":
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation completed")
         print(dataingestionartifact)
+
         data_validation_config=DataValidationConfig(training_pipeline_config)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiate the DataValidation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation completed")
         print(data_validation_artifact)
+
         data_transformation_config=DataTransformationnConfig(training_pipeline_config)
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         logging.info("Data Transformation initiated")
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data Transformation completed")
         print(data_transformation_artifact)
+
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config,data_transformation_artifact)
+        logging.info("Model Training initiated")
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training completed")
+        print(model_trainer_artifact)
+
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 import os,sys
 import numpy as np
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def read_yaml_file(file_path:str)->dict:
     try:
@@ -42,5 +44,47 @@ def save_object(file_path:str,obj:object)->None:
         os.makedirs(dir_name,exist_ok=True)
         with open(file_path,"wb") as file:
             pickle.dump(obj,file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_object(file_path:str)->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file {file_path} dosen't exist")
+        with open(file_path,"rb") as file:
+            print(file)
+            return pickle.load(file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_numpy_array_data(file_path:str)->np.array:
+    try:
+        with open(file_path,"rb") as file:
+            return np.load(file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
     except Exception as e:
         raise NetworkSecurityException(e,sys)
