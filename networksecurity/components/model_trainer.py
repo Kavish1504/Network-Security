73d7fcdba233be17ae4f5c5172c5b3779b866d4a
Kavish Gupta <kavishg36@gmail.com>
2026-02-07 12:13:52 +0530

    ML Flow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -17,6 +17,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier
 )
+import mlflow
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -26,6 +27,17 @@ class ModelTrainer:
 
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    def track_mlflow(self,best_model,classificationmetric):
+            with mlflow.start_run():
+                f1_score=classificationmetric.f1_score
+                precision_score=classificationmetric.precision_score
+                recall_score=classificationmetric.recall_score
+
+                mlflow.log_metric("f1_score ",f1_score)
+                mlflow.log_metric("Precision ",precision_score)
+                mlflow.log_metric("Recall ",recall_score)
+                mlflow.sklearn.log_model(best_model,"model")
 
     def train_model(self,X_train,y_train,X_test,y_test):
         models={
@@ -72,8 +84,11 @@ class ModelTrainer:
         y_train_pred=best_model.predict(X_train)
         classification_train_metric=get_classification_score(y_train,y_train_pred)
 
+        self.track_mlflow(best_model,classification_train_metric)
+
         y_test_pred=best_model.predict(X_test)
         classification_test_metric=get_classification_score(y_test,y_test_pred)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor=load_object(self.data_transformation_artifact.transformed_object_file_path)
         model_dir_path=os.path.dirname(self.model_trainer_config.trained_model_file_path)
